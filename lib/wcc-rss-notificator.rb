71fdb5300833a20e7223952a0b99745aae7de274
cn <chrnicolai@gmail.com>
2011-11-12 23:15:49 +0100

    Rewrite to allow per recipient feeds instead of a global one
    Now all feed options including file location etc emerged to the
    recipients 'rss:' section.

--- lib/wcc-rss-notificator.rb
+++ lib/wcc-rss-notificator.rb
@@ -4,22 +4,66 @@ require 'socket' # only for getting hostname
 require 'yaml'
 
 class RSSNotificator
-	@@feeddb = nil
 	@@tpl = {}
+	@@shutdown_hooks = []
 	
 	# TODO: can we hook into --clean?
 	
-	def initialize
-		# TODO: maybe differentiate by file/html? by recipient
+	def initialize(opts)
+		if opts.is_a?(Hash)
+			@file = opts['file'] || WCC::Conf.file("atom.xml")
+			@db_name = opts['db_name']
+			@num_keep = opts['num_keep'] || 1000
+			@feed = {}
+			if opts['feed'].is_a?(Hash)
+				@feed = {
+					:title => opts['feed']['title'] || "wcc Updates Feed - #{Socket.gethostname}",
+					:subtitle => opts['feed']['subtitle'] || "The newest changes to your watched sites.",
+					:link => opts['feed']['link'],
+					:alt_link => opts['feed']['alternate_link'],
+					:id => opts['feed']['id']
+				}
+			end
+			if @feed[:id].nil?
+				raise ArgumentError, "Missing atom feed ID!"
+			end
+			if @db_name.nil?
+				raise ArgumentError, "Missing db name for atom feed!"
+			end
+		else
+			raise ArgumentError, "No options given for 'rss:' way of a recipient! Need at least db name and feed id."
+		end
+		@@shutdown_hooks << self
+	end
+	
+	def get_feeddb
+		if @feeddb.nil?
+			feeddb_file = WCC::Conf.file("feed-db-#{@db_name}.yml")
+			WCC.logger.debug "Loading feed-db '#{@db_name}' from file '#{feeddb_file}'"
+			if not File.exists?(feeddb_file)
+				WCC.logger.warn "Feed-db '#{@db_name}' does not exist, will create it on save in '#{feeddb_file}'"
+				@feeddb = {'feed' => []}
+			else
+				# may be false if file is empty
+				@feeddb = YAML.load_file(feeddb_file)
+				if not @feeddb.is_a?(Hash)
+					WCC.logger.warn "Feed-db '#{@db_name}' is corrupt, will overwrite it on save"
+					@feeddb = {'feed' => []}
+				end
+			end
+		end
+		@feeddb
 	end
 	
 	def notify!(data)
 		title = self.class.get_tpl(:title, 'rss-title.plain.erb').result(binding)
+		# remove trailing whitespace introduced by template
+		title.strip!
 		content = self.class.get_tpl(:content, 'rss-content.html.erb').result(binding)
-		id = "#{WCC::Conf[:rss_id]}item/#{Time.now.to_f}"
+		id = "#{@feed['id']}item/#{Time.now.to_f}"
 		
 		# append entries to class variable (not thread safe)
-		feed = self.class.get_feeddb['feed']
+		feed = get_feeddb['feed']
 		feed.insert 0, {
 			'title' => title,
 			#'description' => '', # summary
@@ -30,57 +74,36 @@ class RSSNotificator
 		}
 	end
 	
-	def self.parse_conf(conf)
-		if conf.is_a?(Hash)
-			if conf['feed_id'].nil?
-				WCC.logger.fatal "Missing rss feed ID!"
-				return { :rss_id => nil }
-			else
-				return {
-					:rss_title => conf['feed_title'] || "wcc Updates Feed - #{Socket.gethostname}",
-					:rss_subtitle => conf['feed_subtitle'] || "The newest changes to your watched sites.",
-					:rss_link => conf['feed_link'],
-					:rss_alternate_link => conf['feed_alternate_link'],
-					:rss_id => conf['feed_id'],
-					:rss_file => conf['file'] || WCC::Conf.file("atom.xml"),
-					:rss_num_keep => conf['num_keep'] || 1000
-				}
-			end
-		end
-		# no defaults
-		{}
-	end
-	
-	def self.shut_down
-		WCC.logger.info "Write feed-db to file"
+	def shut_down
+		WCC.logger.debug "Writing feed-db '#{@db_name}' to file"
 		
-		feeddb_file = WCC::Conf.file('feed-db.yml')
+		feeddb_file = WCC::Conf.file("feed-db-#{@db_name}.yml")
 		feeddb = get_feeddb
 		# auto-purge
-		feeddb['feed'] = feeddb['feed'].slice(0, WCC::Conf[:rss_num_keep])
+		feeddb['feed'] = feeddb['feed'].slice(0, @num_keep)
 		File.open(feeddb_file, 'w+') do |f| YAML.dump(feeddb, f) end
 		
-		WCC.logger.info "Generate the feed"
+		WCC.logger.info "Generating feed '#{@file}'..."
 		
 		content = RSS::Maker.make('atom') do |m|
-			m.channel.title = WCC::Conf[:rss_title]
-			m.channel.subtitle = WCC::Conf[:rss_subtitle]
+			m.channel.title = @feed[:title]
+			m.channel.subtitle = @feed[:subtitle]
 			m.channel.author = "web change checker (aka wcc) #{WCC::VERSION}"
-			if not WCC::Conf[:rss_link].nil?
+			if not @feed[:link].nil?
 				m.channel.links.new_link do |li|
 					li.rel = "self"
 					li.type = "application/atom+xml"
-					li.href = WCC::Conf[:rss_link]
+					li.href = @feed[:link]
 				end
 			end
-			if not WCC::Conf[:rss_alternate_link].nil?
+			if not @feed[:alternate_link].nil?
 				m.channel.links.new_link do |li|
 					li.rel = "alternate"
 					li.type = "text/html"
-					li.href = WCC::Conf[:rss_alternate_link]
+					li.href = @feed[:alternate_link]
 				end
 			end
-			m.channel.id = WCC::Conf[:rss_id]
+			m.channel.id = @feed[:id]
 			m.channel.updated = Time.now.to_s
 			m.items.do_sort = true # sort items by date
 			
@@ -105,27 +128,15 @@ class RSSNotificator
 			end
 		end
 		
-		File.open(WCC::Conf[:rss_file], 'w+') do |f| f.write(content.to_s) end
+		File.open(@file, 'w+') do |f| f.write(content.to_s) end
 	end
 	
-	def self.get_feeddb
-		if @@feeddb.nil?
-			feeddb_file = WCC::Conf.file('feed-db.yml')
-			if not File.exists?(feeddb_file)
-				WCC.logger.warn "Feed-db file not found, will create one on save."
-				@@feeddb = {'feed' => []}
-			else
-				# may be false if file is empty
-				@@feeddb = YAML.load_file(feeddb_file)
-				if not @@feeddb.is_a?(Hash)
-					WCC.logger.warn "Feed-db is corrupt, will overwrite it on save."
-					@@feeddb = {'feed' => []}
-				end
-			end
-		end
-		@@feeddb
-	end
+	def self.parse_conf(conf); {} end
 	
+	def self.shut_down
+		@@shutdown_hooks.each do |s| s.shut_down end
+	end
+
 	def self.get_tpl(id, name)
 		if @@tpl[id].nil?
 			@@tpl[id] = WCC::Prog.load_template(name)
